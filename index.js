f53bc6f366330bc7832796fe2ffab50ad3a163a8
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-03-25 13:02:55 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    console.log(customerName);
+    return customerName;
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavouriteCustomer = 'Nancy';
+function changeLeastFavoriteCustomer() {
+    leastFavouriteCustomer = 'Mary';
+}
+changeLeastFavoriteCustomer()
